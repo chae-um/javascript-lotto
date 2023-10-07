a7f6dbb0218837fac4287ae67742f7aff0625397
손진영 <sm099@naver.com>
2023-10-07 15:45:21 +0900

    chore(App): eslint, prettier 설정
    1. airbnb 컨벤션을 위한 eslint 파일 추가(.eslintrc.js)
    
    2. 미션 진행을 위한 prettier 설정파일 추가(.prettierrc)

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+    'consistent-return': ['off'],
+    // 들여쓰기 깊이 제한
+    'max-depth': ['error', 2],
+    // 함수의 길이 제한
+    'max-lines-per-function': ['error', { max: 15 }],
+  },
+};
